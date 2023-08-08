613a27cbbbdc38e18d7bc088bf8bfb4d046a6dd8
Vedant Panchal <vedantpanchal12345@gmail.com>
2023-08-08 19:42:51 +0530

    navbar chota

--- client/src/components/Report.jsx
+++ client/src/components/Report.jsx
@@ -350,7 +350,7 @@ const ReportBody = ({ data, risk }) => {
           </p>
         </div>
       </div>
-      {data && <TransactionsTable txs={data.txs} />}
+      {data && data.txs && <TransactionsTable txs={data.txs} />}
     </div>
   );
 };
--- client/src/components/navbar2/navbar2.js
+++ client/src/components/navbar2/navbar2.js
@@ -7,8 +7,8 @@ import { FaFileCsv } from "react-icons/fa";
 
 const Navbar2 = () => {
   return (
-    <nav className="nav2">
-      <div className="icons">
+    <nav className="nav2 t-h-[30px]">
+      <div className="icons t-h-[30px]">
         <AiFillSave className="icon" />
         <RiArrowGoBackFill className="icon" />
         <BiSolidHide className="icon" />
