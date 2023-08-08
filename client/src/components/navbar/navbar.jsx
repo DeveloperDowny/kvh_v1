33531bc1f36b8f971c064fb1ded12c279d0c798f
Aakanksha <aakankshadhawale02@gmail.com>
2023-08-08 19:11:09 +0530

    navbar updated

--- client/src/components/navbar/navbar.jsx
+++ client/src/components/navbar/navbar.jsx
@@ -2,22 +2,59 @@ import React, { useEffect, useState } from "react";
 import "./navbar.css";
 import searchBar from "../searchbar/searchbar";
 import { CgProfile } from "react-icons/cg";
-import { GoSearch } from "react-icons/go";
-import { useSelector } from "react-redux";
+// import { GoSearch } from "react-icons/go";
+// import { useSelector } from "react-redux";
 import {
   Button,
   Input,
   InputGroup,
-  InputLeftElement,
+  InputLeftAddon,
+  InputRightElement,
   Stack,
 } from "@chakra-ui/react";
 import { SearchIcon } from "@chakra-ui/icons"; // Assuming you have imported Chakra's icons
 
 // import styled from "styled-components";
 
+const regexes = {
+  btc: [
+    /^1[a-zA-Z0-9]{25,33}$/,
+    /^3[a-zA-Z0-9]{25,33}$/,
+    /^bc1[a-zA-Z0-9]{23,42}$/,
+    /^bc1p[a-zA-Z0-9]{23,42}$/,
+  ],
+  eth: [/^0x[a-fA-F0-9]{40}$/],
+  xmr: [/^(4|8)[1-9A-Za-z]{94}$/],
+  ada: [
+    /^Ae2[1-9A-HJ-NP-Za-km-z]+$/,
+    /^DdzFF[1-9A-HJ-NP-Za-km-z]+$/,
+    /^addr1[a-z0-9]+$/,
+    /^stake1[a-z0-9]+$/,
+  ],
+  tron: [/^T[A-HJ-NP-Za-km-z1-9]{33}$/],
+  sol: [/^[1-9A-HJ-NP-Za-km-z]{32,44}$/],
+  ton: [/^0:[a-z0-9]{64}$/, /^[a-zA-Z0-9\-\_]{48}$/, /^\w\s\w\s\w$/],
+};
+
 const Navbar = () => {
   // const userData = useSelector((state) => state.auth.authData);
   const [userName, setUserName] = useState("");
+  const [cryptoType, setCryptoType] = useState("");
+
+  const handleInputChange = (e) => {
+    const inputValue = e.target.value;
+
+    // Iterate through the regex patterns and check if the input matches any of them
+    for (const type in regexes) {
+      if (regexes[type].some((pattern) => pattern.test(inputValue))) {
+        setCryptoType(type); // Set the matched crypto type in state
+        return; // Break the loop once a match is found
+      }
+    }
+
+    setCryptoType(""); // Reset the crypto type if no match is found
+    console.log(cryptoType);
+  };
 
   useEffect(() => {
     const userData = JSON.parse(localStorage.getItem("profile"));
@@ -41,21 +78,26 @@ const Navbar = () => {
       <div className="t-flex t-justify-center">
         <div className="t-flex t-items-center t-w-[500px]">
           <InputGroup>
-            <InputLeftElement pointerEvents="none">
-              <SearchIcon color="gray.300" />
-            </InputLeftElement>
+            <InputLeftAddon
+              children={cryptoType}
+              borderRadius="100px 0 0 100px"
+              // onChange={handleInputChange}
+            />
             <Input
               type="text"
               // placeholder="Search here"
               placeholder="Enter Crypto Address Here..."
               background={"white"}
               borderRadius={100}
-              onChange={(e) => {
-                const val = e.target.value;
-                console.log("in on change : ", e.target.value);
-                // TODO: find which network the address belongs to
-              }}
+              // onChange={(e) => {
+              //   const val = e.target.value;
+              //   console.log("in on change : ", e.target.value);
+              // TODO: find which network the address belongs to
+              onChange={handleInputChange}
             />
+            <InputRightElement pointerEvents="none">
+              <SearchIcon color="gray.300" />
+            </InputRightElement>
           </InputGroup>
         </div>
       </div>
@@ -122,3 +164,6 @@ const Navbar = () => {
 };
 
 export default Navbar;
+
+// 1Awyd1QWR5gcfrn1UmL8dUBj2H1eVKtQhg - btc
+// mswUGcPHp1YnkLCgF1TtoryqSc5E9Q8xFa - sol
