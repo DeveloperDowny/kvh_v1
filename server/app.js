aa6704f9f3bfd525bbdd9f7ba060d806f31beb22
Stephen Vaz <stephenvaz2003@gmail.com>
2023-08-09 13:50:28 +0530

    server fix

--- server/app.js
+++ server/app.js
@@ -3,20 +3,33 @@ const cors = require("cors");
 const axios = require("axios");
 const fs = require("fs");
 const app = express();
-
 app.use(cors());
 
 require("./db/Conn");
 const User = require("./models/UserSchema");
 const AddressTracker = require("./models/AddressTracker");
 const auth = require("./middlewares/auth");
-const baseR = express.Router();
 // const { default: auth } = require("./middlewares/auth");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-baseR.use("/explore", require("./Router/Block"));
+
+const baseR = express.Router();
+app.use("/api", (req, res, next) => {
+  // Allow requests from any origin
+  // console.log("test set in req", req.body);
+
+  res.header('Access-Control-Allow-Origin', '*');
+  // Allow the following HTTP methods
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  // Allow these headers to be sent
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+}, baseR);
+
+
+baseR.use("/explore", auth, require("./Router/Block"));
 app.use("/auth", require("./Router/Auth"));
 baseR.use("/market", require("./Router/Market"));
 
@@ -30,6 +43,7 @@ baseR.use("/market", require("./Router/Market"));
 //   next();
 // }, require("./Router/Test"));
 
+
 // app.use((req, res, next) => {
 //   // Allow requests from any origin
 
@@ -40,6 +54,7 @@ baseR.use("/market", require("./Router/Market"));
 //   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
 //   next();
 // });
+
 
 const PORT = 5000;
 app.listen(PORT, () => {
