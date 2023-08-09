d366655f615c64801267a227847b5e164577b550
Aakanksha <aakankshadhawale02@gmail.com>
2023-08-09 16:00:36 +0530

    trust

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  // baseURL: "https://kvh.serveo.net",
-  baseURL: "http://localhost:5000",
+  baseURL: "https://kvh2.serveo.net",
+  // baseURL: "http://localhost:5000",
 });
 
 API.interceptors.request.use((req) => {
